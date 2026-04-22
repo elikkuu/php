4b241479579884ed7aa548e660ef2a36de549c7b
陳宣彤 <a0905457126@gmail.com>
2026-04-22 16:44:22 +0800

    10.login.php

--- 10.login.php
+++ 10.login.php
@@ -1,21 +1,17 @@
 <?php
-   # 連接資料庫
+   
    $link = mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
 
-   # 執行查詢
    $res = mysqli_query($link, "SELECT * FROM user");
 
-   # 設定登入判斷初始值
    $isLogin = FALSE;
 
-   # 逐筆比對帳號密碼
    while ($data = mysqli_fetch_array($res)) {
      if (($_POST["id"] == $data["id"]) && ($_POST["pwd"] == $data["pwd"])) {
        $isLogin = TRUE;
      }
    }
 
-   # 判斷登入結果
    if ($isLogin) {
      session_start();
      $_SESSION["id"] = $_POST["id"];
